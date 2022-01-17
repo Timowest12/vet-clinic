abaf87de3f9d10d43f16a93c48e55aeb9012e4e2
Timo <timowester@gmail.com>
2022-01-17 23:36:55 +0100

    Add new tables

--- schema.sql
+++ schema.sql
@@ -8,3 +8,16 @@ CREATE TABLE animals(
 );
 
 ALTER TABLE animals ADD species varchar(255);
+
+
+--owners table
+CREATE TABLE owners(
+   id SERIAL PRIMARY KEY,
+   full_name VARCHAR(100),
+   age INT
+);
+--species table
+CREATE TABLE species(
+   id SERIAL PRIMARY KEY,
+   name VARCHAR(100)
+   );
