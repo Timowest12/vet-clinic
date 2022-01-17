ff8afb053b71a84bcc2ecfad8391e6a0852df62d
Timo <timowester@gmail.com>
2022-01-17 18:26:36 +0100

    add extra rows

--- data.sql
+++ data.sql
@@ -7,3 +7,9 @@ INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg) VALU
 INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES('Devimon',date '2017-03-1
 2',5,TRUE,11);
 
+INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,eight_kg) VALUES('Charmander',date '2020-02-08',0,FALSE,-11);
+INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,eight_kg) VALUES('Plantmon',date '2022-11-15',2,TRUE,-5.7);
+INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,eight_kg) VALUES('Squirtle',date '1993-02-02',3,TRUE,-12.13);
+INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,eight_kg) VALUES('Angemon',date '2005-04-12',1,FALSE,-45);
+INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,eight_kg) VALUES('Boarmon',date '2005-6-07',7,TRUE,20.4);
+INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,eight_kg) VALUES('Blossom',date '1998-10-13',3,TRUE,17);
