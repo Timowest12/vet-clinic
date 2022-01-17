478875c45f30a7ae758c44ce738d7c7599147289
Timo <timowester@gmail.com>
2022-01-17 09:50:36 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,14 +1,1 @@
-CREATE TABLE IF NOT EXISTS public."Animals"
-(
-    id integer NOT NULL,
-    name text COLLATE pg_catalog."default",
-    date_of_birth date,
-    escape_attempts boolean,
-    weight numeric(10,2),
-    CONSTRAINT "Animals_pkey" PRIMARY KEY (id)
-)
 
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public."Animals"
-    OWNER to postgres;
