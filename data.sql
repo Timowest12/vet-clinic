517ba06c8c07a92adc7adfaed02eeefa64fdabae
Timo <timowester@gmail.com>
2022-01-18 20:07:57 +0100

    insert owners

--- data.sql
+++ data.sql
@@ -13,3 +13,33 @@ INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,eight_kg) VALUE
 INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,eight_kg) VALUES('Angemon',date '2005-04-12',1,FALSE,-45);
 INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,eight_kg) VALUES('Boarmon',date '2005-6-07',7,TRUE,20.4);
 INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,eight_kg) VALUES('Blossom',date '1998-10-13',3,TRUE,17);
+
+
+--insert owners
+--Sam Smith 34 years old.
+INSERT INTO owners (full_name, age)
+VALUES ('Sam Smith', 34);
+--Jennifer Orwell 19 years old.
+INSERT INTO owners (full_name, age)
+VALUES (
+        'Jennifer Orwell',
+        19
+    );
+--Bob 45 years old.
+INSERT INTO owners (full_name, age)
+VALUES ('Bob', 45);
+--Melody Pond 77 years old.
+INSERT INTO owners (full_name, age)
+VALUES ('Melody Pond', 77);
+--Dean Winchester 14 years old.
+INSERT INTO owners (full_name, age)
+VALUES (
+        'Dean Winchester',
+        14
+    );
+--Jodie Whittaker 38 years old.
+INSERT INTO owners (full_name, age)
+VALUES (
+        'Jodie Whittaker',
+        38
+    );
