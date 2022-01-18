7c9dbdc3641a2b2cb1740b01ab3b647e98693d35
Timo <timowester@gmail.com>
2022-01-18 20:02:40 +0100

    add foreighn keys

--- schema.sql
+++ schema.sql
@@ -21,3 +21,14 @@ CREATE TABLE species(
    id SERIAL PRIMARY KEY,
    name VARCHAR(100)
    );
+   
+   --Make sure that id is set as autoincremented PRIMARY KEY- DONE
+
+--drop species column
+ALTER TABLE animals DROP COLUMN species;
+--Add column species_id as a froreighn key
+ALTER TABLE animals ADD COLUMN species_id INT,
+ADD CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id);
+--Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE animals ADD COLUMN owner_id INT,
+ADD CONSTRAINT fk_owners FOREIGN KEY(owner_id) reference to owners(id);
